db132c0c5a689c89d73df4ca4c4f171af6c8559d
vadim <pld000@yandex.ru>
2017-06-26 10:38:50 +0300

    made page layout, added mdi, started work on slider

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,6 +3,18 @@
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
 
-$(function() {
+$(function () {
+  var displayOffset = 300;
+  var slide2 = $('.slide2');
+  var slide2Top = slide2.offset().top - displayOffset;
 
+  $(document).on('scroll', function () {
+    if ($(document).scrollTop() > slide2Top) {
+      $('.image-backstretch > div').removeClass('active');
+      $('.image-slide-2').addClass('active');
+    } else {
+      $('.image-backstretch > div').removeClass('active');
+      $('.image-slide-1').addClass('active');
+    }
+  });
 });
